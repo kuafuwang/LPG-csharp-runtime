6c5356b1ab2bc078b831ac40c42e427ff71b332b
kuafuwang <731784510@qq.com>
2021-07-31 20:07:05 +0800

    1.Update the errorMsgText.

--- LPG2.Runtime/ParseErrorCodes.cs
+++ LPG2.Runtime/ParseErrorCodes.cs
@@ -21,20 +21,20 @@ public abstract class  ParseErrorCodes
                              NO_MESSAGE_CODE = 13;
 
     public  static string[] errorMsgText= {
-        /* LEX_ERROR_CODE */                       ("ParseErrorCodes.LEX_ERROR_CODE"), //$NON-NLS-1$
-        /* ERROR_CODE */                           ("ParseErrorCodes.ERROR_CODE"), //$NON-NLS-1$
-        /* BEFORE_CODE */                          ("ParseErrorCodes.BEFORE_CODE"), //$NON-NLS-1$
-        /* INSERTION_CODE */                       ("ParseErrorCodes.INSERTION_CODE"), //$NON-NLS-1$
-        /* INVALID_CODE */                         ("ParseErrorCodes.INVALID_CODE"), //$NON-NLS-1$
-        /* SUBSTITUTION_CODE, SECONDARY_CODE */    ("ParseErrorCodes.SUBSTITUTION_CODE"), //$NON-NLS-1$
-        /* DELETION_CODE */                        ("ParseErrorCodes.DELETION_CODE"), //$NON-NLS-1$
-        /* MERGE_CODE */                           ("ParseErrorCodes.MERGE_CODE"), //$NON-NLS-1$
-        /* MISPLACED_CODE */                       ("ParseErrorCodes.MISPLACED_CODE"), //$NON-NLS-1$
-        /* SCOPE_CODE */                           ("ParseErrorCodes.SCOPE_CODE"), //$NON-NLS-1$
-        /* EOF_CODE */                             ("ParseErrorCodes.EOF_CODE"), //$NON-NLS-1$
-        /* INVALID_TOKEN_CODE, ERROR_RULE_ERROR */ ("ParseErrorCodes.INVALID_TOKEN_CODE"), //$NON-NLS-1$
-        /* ERROR_RULE_WARNING */                   ("ParseErrorCodes.ERROR_RULE_WARNING"), //$NON-NLS-1$
-        /* NO_MESSAGE_CODE */                      ("ParseErrorCodes.NO_MESSAGE_CODE") //$NON-NLS-1$
+        /* LEX_ERROR_CODE */                       "unexpected character ignored", //$NON-NLS-1$
+        /* ERROR_CODE */                           "parsing terminated at this token", //$NON-NLS-1$
+        /* BEFORE_CODE */                          " inserted before this token", //$NON-NLS-1$
+        /* INSERTION_CODE */                       " expected after this token", //$NON-NLS-1$
+        /* INVALID_CODE */                         "unexpected input discarded", //$NON-NLS-1$
+        /* SUBSTITUTION_CODE, SECONDARY_CODE */    " expected instead of this input", //$NON-NLS-1$
+        /* DELETION_CODE */                        " unexpected token(s) ignored", //$NON-NLS-1$
+        /* MERGE_CODE */                           " formed from merged tokens", //$NON-NLS-1$
+        /* MISPLACED_CODE */                       "misplaced construct(s)", //$NON-NLS-1$
+        /* SCOPE_CODE */                           " inserted to complete scope", //$NON-NLS-1$
+        /* EOF_CODE */                             " reached after this token", //$NON-NLS-1$
+        /* INVALID_TOKEN_CODE, ERROR_RULE_ERROR */ " is invalid", //$NON-NLS-1$
+        /* ERROR_RULE_WARNING */                   " is ignored", //$NON-NLS-1$
+        /* NO_MESSAGE_CODE */                      "" //$NON-NLS-1$
     };
 
 }
