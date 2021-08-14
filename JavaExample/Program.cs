5187a154afc179f3f084cca31b76a52af0a8ed8c
kuafuwang <731784510@qq.com>
2021-08-14 19:52:37 +0800

    1.Add JavaExample.

---
+++ JavaExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using LpgJava;
+
+namespace JavaExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _lexer = new JavaLexer("D:/workspace/New_Parser/lpg_java/src/test/test2.java"); // Create the lexer
+            var parser = new JavaParser(_lexer.getILexStream());
+            _lexer.printTokens = true;
+            _lexer.lexer(parser.getIPrsStream());
+
+            var ast = parser.parser();
+            if (ast != null)
+            {
+                Console.WriteLine("成功");
+            }
+            else
+            {
+                Console.WriteLine("失败");
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -15,4 +15,4 @@ C# runtime for The LALR parser generator (LPG)
 
 
 ## About LPG
-The LALR parser generator ([LPG]( https://github.com/kuafuwang/LPG2 )) is a tool for developing scanners and parsers written in C#,Java, C++ or C. Input is specified by BNF rules. LPG supports backtracking (to resolve ambiguity), automatic AST generation and grammar inheritance.
+The LALR parser generator ([LPG]( https://github.com/kuafuwang/LPG2 )) is a tool for developing scanners and parsers written in TypeScript, C#,Java, C++ or C. Input is specified by BNF rules. LPG supports backtracking (to resolve ambiguity), automatic AST generation and grammar inheritance.
